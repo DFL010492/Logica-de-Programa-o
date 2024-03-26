d7d7cdda48c40547919172a8012fa13d19cccd9f
DFL010492 <161463053+DFL010492@users.noreply.github.com>
2024-03-26 11:12:16 -0300

    Add files via upload

---
+++ Aula122.js
@@ -0,0 +1,20 @@
+const input = require("fs").readFileSync("stdin122","utf8");
+
+PALAVRA = input.split();
+
+if (PALAVRA === 'vertebrado') {
+PALAVRA = input.split();
+
+    if (PALAVRA === 'ave') {
+        PALAVRA = input.split();
+    }
+        if (PALAVRA === 'carnivoro') {
+            PALAVRA = input.split();
+            animal = PALAVRA === ' carnivoro' ? 'aguia' : 'aguia'
+        }
+        
+        else {
+            animal = PALAVRA === 'onivoro' ? 'pomba' : 'pomba'
+        }
+}
+   
