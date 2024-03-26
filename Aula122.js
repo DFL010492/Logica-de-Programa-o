765b5ee6d061962f942da9429875a8b0f07e8f41
DFL010492 <161463053+DFL010492@users.noreply.github.com>
2024-03-26 14:00:30 -0300

    Update Aula122.js

--- Aula122.js
+++ Aula122.js
@@ -3,18 +3,69 @@ const input = require("fs").readFileSync("stdin122","utf8");
 PALAVRA = input.split();
 
 if (PALAVRA === 'vertebrado') {
-PALAVRA = input.split();
+    PALAVRA = input.split();
 
     if (PALAVRA === 'ave') {
         PALAVRA = input.split();
     }
+
         if (PALAVRA === 'carnivoro') {
             PALAVRA = input.split();
-            animal = PALAVRA === ' carnivoro' ? 'aguia' : 'aguia'
+            animal = PALAVRA === ' carnivoro' ? 'aguia' : 'aguia';
         }
         
         else {
-            animal = PALAVRA === 'onivoro' ? 'pomba' : 'pomba'
+            PALAVRA = input.split();
+            animal = PALAVRA === 'onivoro' ? 'pomba' : 'pomba';
+        }     
+
+    else {
+        PALAVRA = input.split();
+        animal = PALAVRA === 'mamifero';
+    }
+
+        if (PALAVRA === 'onivoro') {
+            PALAVRA = input.split();
+            animal = PALAVRA === 'onivorro' ? 'homem' : 'homem';
+        }
+
+        else {
+            PALAVRA = input.split();
+            animal = PALAVRA === 'herbivoro' ? 'vaca' : 'vaca';
         }
 }
-   
+
+else {
+    PALAVRA = input.split();
+    animal = PALAVRA === 'invertebrado'
+}
+
+    if (PALAVRA === 'inseto'); {
+        PALAVRA = input.split();
+        animal = PALAVRA === 'inseto';
+    }
+
+        if (PALAVRA === 'hematofago') {
+            PALAVRA = input.split();
+            animal = PALAVRA === 'hematofago' ? 'pulga' : 'pulga';
+        }
+
+        else {
+            PALAVRA = input.split();
+            animal = PALAVRA === 'herbivoro' ? 'lagarta' : 'lagarta';
+        }
+
+    else {
+        PALAVRA = input.split();
+        animal = PALAVRA === 'anelideo';
+    }
+        if (PALAVRA === 'hematofago') {
+            PALAVRA = input.split();
+            animal = PALAVRA === 'hematofago' ? 'sanguessuga' : 'sanguessuga'
+        }
+
+        else {
+            PALAVRA = input.split();
+            animal = PALAVRA === 'onivoro' ? 'minhoca' : 'minhoca'
+        }
+console.log(animal)
