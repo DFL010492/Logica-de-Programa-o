f28605a99b14782b4f1898299ce1330afd4477a7
DFL010492 <161463053+DFL010492@users.noreply.github.com>
2024-03-26 23:38:02 -0300

    Update Aula122.js

--- Aula122.js
+++ Aula122.js
@@ -1,76 +1,31 @@
-const input = require("fs").readFileSync("STANDART52","utf8");
+const input = require("fs").readFileSync("STANDART52", "utf8");
 
-PALAVRA = input.split('');
+palavra = input.split(/\s+/);
+console.log(palavra[2]);
 
 
-if (PALAVRA === 'vertebrado') {
-    PALAVRA = input.split();
-
-    if (PALAVRA === 'ave') {
-        PALAVRA = input.split();
-    
-        if (PALAVRA === 'carnivoro') {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'carnivoro' ? 'aguia' : 'aguia';
-        
-        if ( PALAVRA === 'onivoro') {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'onivoro' ? 'pomba' : 'pomba';
-        }
-        }
+if (palavra === 'vertebrado') {
+    console.log(palavra[2]);
+    palavra = input.split(/\s+/);
+    if (palavra === ave) {
+        palavra = input.split(/\s+/);
+        animal = palavra === 'carnivoro' ? 'aguia' : 'pomba';
+} 
+    else {
+        palavra = input.split(/\s+/);
+        animal = palavra === 'onivoro' ? 'homem' : 'vaca';
     }
 }
-    if (PALAVRA === 'mamifero') {
-        PALAVRA = input.split();
-        animal = PALAVRA === 'mamifero';
-
-
-        if (PALAVRA === 'onivoro') {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'onivorro' ? 'homem' : 'homem';
-        }
-
-        else {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'herbivoro' ? 'vaca' : 'vaca';
-        }
-}
-
 else {
-    PALAVRA = input.split();
-    animal = PALAVRA === 'invertebrado'
+    palavra = input.split(/\s+/);
+    if (palavra === 'inseto') {
+        palavra = input.split(/\s+/);
+        animal = palavra === 'hematofago' ? 'pulga' : 'lagarta';
+} 
+    else {
+        palavra = input.split(/\s+/);
+        animal = palavra === 'hematofago' ? 'sanguesuga' : 'minhoca';
+    }
 }
-
-    if (PALAVRA === 'inseto'); {
-        PALAVRA = input.split();
-        animal = PALAVRA === 'inseto';
-    }
-
-        if (PALAVRA === 'hematofago') {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'hematofago' ? 'pulga' : 'pulga';
-        }
-
-        else {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'herbivoro' ? 'lagarta' : 'lagarta';
-        }
-    
-
-    if (PALAVRA === 'anelideo') {
-        PALAVRA = input.split();
-        animal = PALAVRA === 'anelideo';
-    }
-
-        if (PALAVRA === 'hematofago') {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'hematofago' ? 'sanguessuga' : 'sanguessuga'
-        }
-
-        else {
-            PALAVRA = input.split();
-            animal = PALAVRA === 'onivoro' ? 'minhoca' : 'minhoca'
-        }
-    
 
 console.log(animal);
